073922507a6d28302a4f633d86834d65ff211ad8
YKrasnobokyi <krasnobokiy.yuriy@gmail.com>
2026-03-10 11:00:47 +0200

    add simple products routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import healthRoutes from "./routes/health.routes.js";
+import productsRoutes from "./routes/products.routes.js";
 import { swaggerUi, swaggerSpec } from "./config/swagger.js";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/", healthRoutes);
+app.use("/products", productsRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
